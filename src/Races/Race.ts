86c2873f33e27674719edcb7fda1ab308dd5455e
Lucas <lucaschmain@gmail.com>
2022-11-18 15:50:07 -0300

    Completing Requirement 01

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,3 +1,17 @@
-export default class Race {
-  constructor(private name: string, private dexterity: number) {}
+export default abstract class Race {  
+  constructor(private _name: string, private _dexterity: number) {}
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  public abstract get maxLifePoints(): number;
+
+  public static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
 }
